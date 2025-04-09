b73cea2bd881e77e5861bc53ed770a4bda1be02d
Your Name <you@example.com>
2025-04-09 16:01:15 +0530

    let vs var

--- index.js
+++ index.js
@@ -24,26 +24,103 @@
 // Array.prototype.lol = function(){
 //     console.log("this array injecter")
 // }
-let addtwonumber = (x,y)=>{
-    // console.log(x+y)
-    return x+y
+// let addtwonumber = (x,y)=>{
+//     // console.log(x+y)
+//     return x+y
+
+// }
+
+// for(let i = 0;i<=10;i++){
+//     let result = addtwonumber(i,i*2)
+
+// }
+// // console.log(addtwonumber(89,90))
+
+// // vio.hitsh()
+// // let g = ['so','lo','uolo']
+// // let arr =['i ','am',...g]
+// function name  (...arr){
+//     arr.reduce((total,nums)=>{ 
+//         console.log(nums)
+
+//      })
+//     console.log()
+
+// }
+// variable scope in function 
+//  function test (){
+//     let y = "shashnak"
+
+//     console.log("avaible here ",y)
+
+    
+// }
+// let globalscope = "here we"
+// function testavailblity(){
+    
+//     if(true){
+//         var x = "local"
+
+//     }
+//     console.log(x)
+//     console.log(uilo)
+
+//     var uilo = 89
+
+// }
+// function testavailblity(){
+    
+//     if(true){
+//         var x = "local"
+
+//     }
+    
+//     console.log(x)
+//     console.log(globalscope)
+
+    
+
+// }
+// function testavailblity(){
+//     let globalscope = "shashank vaish"
+//     console.log(globalscope)
+// }
+// //constant scope 
+// function doingstuff(x){
+//     console.log(x)
+
+// }
+// doingstuff("param")
+// function confuseReader(){
+//     x = "guess my scope"// here goes global scope 
+//     console.log("inside the function ",x)
+// }
+// confuseReader()
+// console.log("outside the scope ",x)
+// (()=>{
+//     console.log("here ")
+// })()
+// (()=>{
+//     console.log("we are different type ")
+// })()
+//recursive function 
+
+
+function getrecursion(nr){
+    if(nr == 0){
+    return;
+    }
+
+    console.log(nr)
+    getrecursion(--nr)
+    
 
 }
-
-for(let i = 0;i<=10;i++){
-    let result = addtwonumber(i,i*2)
-
+function logrecursive(nr){
+    console.log('started function ')
 }
-// console.log(addtwonumber(89,90))
-
-// vio.hitsh()
-// let g = ['so','lo','uolo']
-// let arr =['i ','am',...g]
-function name  (...arr){
-    arr.reduce((total,nums)=>{ 
-        console.log(nums)
-
-     })
-    console.log()
-
-}
+getrecursion(3)
+funct
+// testavailblity()
+// console.log(globalscope)
+// console.log('not availble ',y)
